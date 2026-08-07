c0c9ed96df3c49fca9d502a560db7394aec0c9ec
Godspeace Adetayo <adetayogodspeace2003@gmail.com>
2026-08-07 20:36:49 +0100

    added shadow

--- app/properties/list/PropertiesPage.tsx
+++ app/properties/list/PropertiesPage.tsx
@@ -37,7 +37,7 @@ const PropertiesPage = () => {
   }, [searchParams]);
 
   const filtered = properties.filter((p) =>
-    current ? p.state.toLowerCase() === current.toLowerCase() : true
+    current ? p.state.toLowerCase() === current.toLowerCase() : true,
   );
 
   if (filtered.length === 0) {
@@ -52,39 +52,44 @@ const PropertiesPage = () => {
   return (
     <>
       <PropertyToolBar />
+      <div>
+        <Grid
+          columns={{ initial: "1", sm: "3", md: "5" }}
+          className="justify-items-center"
+          gap={"3"}
+        >
+          {properties.map((property) => (
+            <Box maxWidth={"240px"} key={property.id} className="shadow-xl">
+              <Card size={"2"}>
+                <Inset clip={"padding-box"} side={"top"}>
+                  <CldImage
+                    src={property.images[0]?.url}
+                    alt={property.title}
+                    width={240}
+                    height={140}
+                    crop="fill"
+                    className="object-cover w-full h-[140px]"
+                  />
+                </Inset>
 
-      <Grid columns={{ initial: "2", sm: "3", md: "5" }} gap={"3"}>
-        {properties.map((property) => (
-          <Box maxWidth={"240px"} key={property.id}>
-            <Card size={"2"}>
-              <Inset clip={"padding-box"} side={"top"}>
-                <CldImage
-                  src={property.images[0]?.url}
-                  alt={property.title}
-                  width={400}
-                  height={300}
-                  crop="fill"
-                  className="object-cover w-full h-[140px]"
-                />
-              </Inset>
-
-              <Flex direction={"column"}>
-                <Text size="3" weight="bold">
-                  <Link href={`/properties/${property.id}`}>
-                    {property.title}
-                  </Link>
-                </Text>
-                <Text>{property.state}</Text>
-                <Text>{property.category}</Text>
-                <Text>
-                  {property.currency} {property.price}
-                </Text>
-              </Flex>
-              <BookButton property={property} />
-            </Card>
-          </Box>
-        ))}
-      </Grid>
+                <Flex direction={"column"}>
+                  <Text size="3" weight="bold">
+                    <Link href={`/properties/${property.id}`}>
+                      {property.title}
+                    </Link>
+                  </Text>
+                  <Text>{property.state}</Text>
+                  <Text>{property.category}</Text>
+                  <Text>
+                    {property.currency} {property.price}
+                  </Text>
+                </Flex>
+                <BookButton property={property} />
+              </Card>
+            </Box>
+          ))}
+        </Grid>
+      </div>
     </>
   );
 };
