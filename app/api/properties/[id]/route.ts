0fff5a4be7079a97fdd6393c42dc636352b91cb9
Godspeace Adetayo <adetayogodspeace2003@gmail.com>
2026-09-24 21:30:33 +0100

    corrected promise

--- app/api/properties/[id]/route.ts
+++ app/api/properties/[id]/route.ts
@@ -5,11 +5,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const property = await prisma.property.findUnique({
-      where: { id: params.id },
+      where: { id: (await params).id },
       include: { images: true },
     });
 
