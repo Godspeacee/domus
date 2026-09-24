65f1a5d306749c161ab91e58f9650fb3a9eee437
Godspeace Adetayo <adetayogodspeace2003@gmail.com>
2026-09-24 18:14:25 +0100

    database update

--- prisma/clientfile.ts
+++ prisma/clientfile.ts
@@ -1,7 +1,18 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@/app/generated/prisma";
 
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST!,
+  port: Number(process.env.DATABASE_PORT!),
+  user: process.env.DATABASE_USER!,
+  password: process.env.DATABASE_PASSWORD!,
+  database: process.env.DATABASE_NAME!,
+  connectionLimit: 5,
+});
+
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({ adapter });
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
@@ -11,6 +22,9 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
